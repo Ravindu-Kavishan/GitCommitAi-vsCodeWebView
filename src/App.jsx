cf8c5b591fc92f0ccdae6ef0a3fa37fbac467bc1
Ravindu-Kavishan <Priyadarshandmrk.22@uom.lk>
2025-03-20 13:54:47 +0530

    remove assert folder.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import LogIn from "./assets/pages/auth/LogIn";
 import ForgetPassword from "./assets/pages/auth/FogetPassword";
 import OTP from "./assets/pages/auth/OTP";
 import ChangePassword from "./assets/pages/auth/ChangePassword";
+import CommitHistory from "./assets/pages/commitHistory/ComitHistory";
+import ExplainingCommits from "./assets/pages/commitHistory/ExplainingCommits";
 
 export default function App() {
   return (
@@ -18,6 +20,8 @@ export default function App() {
           <Route path ="/forgetpassword" element={<ForgetPassword/>}/>
           <Route path ="/OTP" element={<OTP/>}/>
           <Route path ="/ChangePassword" element={<ChangePassword/>}/>
+          <Route path="/CommitHistory" element={<CommitHistory/>}/>
+          <Route path="/ExplainingCommits" element={<ExplainingCommits/>}/>
         </Routes>
       </BrowserRouter>
     </div>
